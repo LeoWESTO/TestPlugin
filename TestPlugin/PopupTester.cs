91c6d46f5a30e0f837fc9e6beb85f3abf5c7a983
Amir Zagirov <leowestjob2001@gmail.com>
2023-09-26 14:48:31 +0300

    Добавьте файлы проекта.

---
+++ TestPlugin/PopupTester.cs
@@ -0,0 +1,35 @@
+﻿using Resto.Front.Api;
+using System;
+using System.Reactive.Disposables;
+
+namespace TestPlugin
+{
+    internal sealed class PopupTester : IDisposable
+    {
+        private readonly CompositeDisposable subscriptions;
+
+        public PopupTester()
+        {
+            subscriptions = new CompositeDisposable()
+            {
+                // Добавить кнопку на экран заказа, при нажатии на которую выводить PopUp о общем количестве добавленных позиций в заказ
+                PluginContext.Operations.AddButtonToOrderEditScreen("TestPlugin", x =>
+                {
+                    int itemsCount = x.order.Items.Count;
+                    x.vm.ShowOkPopup("TestPlugin", $"Добавлено позиицй: {itemsCount}");
+                }),
+
+                // При создании заказа вывести PopUp с номером заказа и его типом (доставка или обычный)
+                PluginContext.Notifications.OrderChanged.Subscribe(x =>
+                {
+                    PluginContext.Operations.AddNotificationMessage($"Заказ №{x.Entity.Number} типа {x.Entity.OrderType.Name}", "TestPlugin", TimeSpan.FromSeconds(10));
+                }),
+            };
+        }
+
+        public void Dispose()
+        {
+            subscriptions.Dispose();
+        }
+    }
+}
---
+++ TestPlugin/TestPlugin.cs
@@ -0,0 +1,46 @@
+﻿using Resto.Front.Api;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Runtime.Remoting;
+using Resto.Front.Api.Attributes.JetBrains;
+using Resto.Front.Api.Attributes;
+
+namespace TestPlugin
+{
+    [UsedImplicitly]
+    [PluginLicenseModuleId(21005108)]
+    public sealed class TestPlugin : IFrontPlugin
+    {
+        private readonly Stack<IDisposable> subscriptions = new Stack<IDisposable>();
+        public TestPlugin()
+        {
+            PluginContext.Log.Info("Initializing TestPlugin");
+
+            subscriptions.Push(new PopupTester());
+
+            PluginContext.Log.Info("TestPlugin started");
+
+            // При старте плагина вывести PopUp с информацией, что плагин запустился
+            PluginContext.Operations.AddNotificationMessage("Плагин запустился!", "TestPlugin", TimeSpan.FromSeconds(10));
+        }
+
+        public void Dispose()
+        {
+            while (subscriptions.Any())
+            {
+                var subscription = subscriptions.Pop();
+                try
+                {
+                    subscription.Dispose();
+                }
+                catch (RemotingException ex)
+                {
+                    PluginContext.Log.Error($"TestPlugin stopped. Message: {ex.Message}");
+                }
+            }
+
+            PluginContext.Log.Info("TestPlugin stopped");
+        }
+    }
+}
